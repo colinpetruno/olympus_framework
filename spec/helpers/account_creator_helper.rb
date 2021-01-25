499cdaaa417e05820bfa3c1591cd5bec8a72b24e
Kamal Uddin Panhwar <kamalpanhwar@gmail.com>
2021-01-25 22:17:46 +0500

    modifed helper to create admin page so user can login as without admin it will take back to signup page

--- spec/helpers/account_creator_helper.rb
+++ spec/helpers/account_creator_helper.rb
@@ -3,14 +3,14 @@ module AccountCreatorHelper
     return @session_info if @session_info.present?
 
     json = JSON.parse(
-      File.read(
-        Rails.root.join("spec/support/files/google_auth_response.json")
+        File.read(
+            Rails.root.join('spec/support/files/google_auth_response.json')
+          )
       )
-    )
 
     oauth_response = Authentication::OauthResponse.new(json)
     result = Authentication::Processors::AccountCreator.for(oauth_response)
-
+    result[:member][:member_type] = 'application_admin'
     @session_info = Members::SessionInfo.for(result[:member])
     @session_info
   end
@@ -23,20 +23,19 @@ module AccountCreatorHelper
     @_default_company ||= default_session_info.company
   end
 
-  def session_info_custom_data(options={ member: {}, company: {} })
+  def session_info_custom_data(options = { member: {}, company: {} })
     random_id = SecureRandom.uuid
     default_settings = {
-      member: {
-        email: "test_member_#{random_id}@example.com"
-      },
-
-      company: {
-        email_domain: "#{random_id}_example.com",
-        name: "#{random_id}_example.com",
-        auth_credential_id: nil,
-        open_signups: true,
-        provider: "google_oauth2"
-      }
+        member: {
+            email: "test_member_#{random_id}@example.com"
+        },
+        company: {
+            email_domain: "#{random_id}_example.com",
+            name: "#{random_id}_example.com",
+            auth_credential_id: nil,
+            open_signups: true,
+            provider: 'google_oauth2'
+        }
     }
 
     result = Accounts::Creator.for(default_settings.deep_merge(options))
@@ -44,22 +43,22 @@ module AccountCreatorHelper
     Members::SessionInfo.for(result[:member])
   end
 
-  def create_member(attributes={})
+  def create_member(attributes = {})
     email = "pascalfluffy-#{SecureRandom.uuid}@example.com"
 
     Member.create({
-      email: email,
-      company: default_company,
-      profile_attributes: {
         email: email,
         company: default_company,
-        team: default_team,
-        role: :company_admin,
-        timezone: "UTC",
-        given_name: "Pascal-#{(1..100).to_a.sample}",
-        family_name: "Fluffy-#{(1..100).to_a.sample}",
-        external_slug: SecureRandom.uuid
-      }
+        profile_attributes: {
+            email: email,
+            company: default_company,
+            team: default_team,
+            role: :company_admin,
+            timezone: 'UTC',
+            given_name: "Pascal-#{(1..100).to_a.sample}",
+            family_name: "Fluffy-#{(1..100).to_a.sample}",
+            external_slug: SecureRandom.uuid
+        }
     }.deep_merge(attributes))
   end
 end
