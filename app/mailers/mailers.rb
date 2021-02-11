eaf684674248305e3c703d698710a88623fa19fe
Kamal Uddin Panhwar <kamalpanhwar@gmail.com>
2021-02-11 19:19:21 +0500

    modified unnecessary code due to rubocop violation

--- app/mailers/mailers.rb
+++ app/mailers/mailers.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
+# Mail to get template from table
 class Mailers
   def self.load(klass, method_name)
-    mail_template = MailTemplate.where(class_name: klass.to_s, method_name: method_name).first
-    mail_template
+    MailTemplate.where(class_name: klass.to_s, method_name: method_name).first
   end
 end
