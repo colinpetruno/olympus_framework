21bc7ba9feadf641ac3ea06803dc2c16bea6fa4b
Kamal Uddin Panhwar <kamalpanhwar@gmail.com>
2021-02-11 17:31:57 +0500

    Added template to take markdown and make it html

--- app/models/mail_template.rb
+++ app/models/mail_template.rb
@@ -1,12 +1,17 @@
+# frozen_string_literal: true
+
+# model for mail template create in db and convert from markdown
 class MailTemplate < ApplicationRecord
   validates :method_name, presence: true
   validates :class_name, presence: true
   validates :content, presence: true
   validates :subject, presence: true
 
-  def to_html
-    template = Redcarpet::Render::HTML.new(prettify: true)
-    markdown = Redcarpet::Markdown.new(template)
-    markdown.render(content)
+  def to_html(context)
+    template = ERB.new content
+    template_result = template.result(context)
+    document = Redcarpet::Render::HTML.new(prettify: true)
+    markdown = Redcarpet::Markdown.new(document)
+    markdown.render(template_result)
   end
 end
